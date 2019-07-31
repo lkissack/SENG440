ac866271be260c9af355f33a0a4c2e833f738293
hpfletch <35049762+hpfletch@users.noreply.github.com>
2019-07-30 20:34:58 -0700

    Add files via upload

--- FIR/main.c
+++ FIR/main.c
@@ -37,6 +37,8 @@ void FIR(int16_t *filter_coeffs, int16_t *input, int16_t *output, int length) //
     register int16_t *x; //pointer to input samples
     register int32_t temp1 = 0;
     register int32_t temp2 = 0;
+    register int32_t temp3 = 0;
+    register int32_t temp4 = 0;
   
 //copy the samples out of memory into the buffer   
  memcpy( &insamp[FILT_LENGTH - 1], input, length * sizeof(int16_t));
@@ -49,54 +51,43 @@ void FIR(int16_t *filter_coeffs, int16_t *input, int16_t *output, int length) //
 
     acc = (1 << 14);// preload rounding constant
 
-	temp1 = (int32_t)(*h++)*(int32_t)(*x--);
-	temp2 = (int32_t)(*h++)*(int32_t)(*x--);
-	//temp1 += (1<<6); //rounding 
-    //temp1 >>= 7;
+     temp1 = (int32_t)(*h++)*(int32_t)(*x--); //perform multiplication and add to accumulator
+            temp2 = (int32_t)(*h++)*(int32_t)(*x--); //perform multiplication and add to accumulator
+            temp3 = (int32_t)(*h++)*(int32_t)(*x--); 
+            temp4 = (int32_t)(*h++)*(int32_t)(*x--); 
+
+            temp1 += (int32_t)(*h++)*(int32_t)(*x--);
+            temp2 += (int32_t)(*h++)*(int32_t)(*x--); //perform multiplication and add to accumulator
+            temp3 += (int32_t)(*h++)*(int32_t)(*x--);
+            temp4 += (int32_t)(*h++)*(int32_t)(*x--);
+
   
 
         for (k = 0; k < ((FILT_LENGTH)>>3); k++)
         {  
             acc += temp1;
             acc += temp2;
+            acc += temp3;
+            acc += temp4;
+
 
             temp1 = (int32_t)(*h++)*(int32_t)(*x--); //perform multiplication and add to accumulator
-            
-            //temp2 += (1<<4); //rounding 
-            //temp2 >>= 5;     //shift
-
             temp2 = (int32_t)(*h++)*(int32_t)(*x--); //perform multiplication and add to accumulator
-            
-            //temp1 += (1<<4); //rounding 
-            //temp1 >>= 5;     //shift
+            temp3 = (int32_t)(*h++)*(int32_t)(*x--); 
+            temp4 = (int32_t)(*h++)*(int32_t)(*x--); 
 
-            //acc = acc + temp1;
-           temp1 += (int32_t)(*h++)*(int32_t)(*x--);
-            //temp2 += (1<<4); //rounding 
-            //temp2 >>= 5;     //shift
-
-            //acc = acc + temp2;
-           temp2 += (int32_t)(*h++)*(int32_t)(*x--); //perform multiplication and add to accumulator
-            
-           // temp1 += (1<<4); //rounding 
-            //temp1 >>= 5;     //shift
-           temp1 += (int32_t)(*h++)*(int32_t)(*x--);
-            //temp2 += (1<<4); //rounding 
-            //temp2 >>= 5;     //shift
-
-            //acc = acc + temp2;
-           temp2 += (int32_t)(*h++)*(int32_t)(*x--);
             temp1 += (int32_t)(*h++)*(int32_t)(*x--);
-            //temp2 += (1<<4); //rounding 
-            //temp2 >>= 5;     //shift
-
-            //acc = acc + temp2;
-           temp2 += (int32_t)(*h++)*(int32_t)(*x--);
-
+            temp2 += (int32_t)(*h++)*(int32_t)(*x--); //perform multiplication and add to accumulator
+            temp3 += (int32_t)(*h++)*(int32_t)(*x--);
+            temp4 += (int32_t)(*h++)*(int32_t)(*x--);
+ 
+         
         }
         
         acc += temp1;
         acc += temp2;
+        acc += temp3;
+        acc += temp4;
 
 
        /*  if( acc > upper_limit){ //check if accumulator has saturated
