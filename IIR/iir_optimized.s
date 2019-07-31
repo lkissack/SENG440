8a468a6968c66be6b3b9640a9eac770b82f83a75
lkissack <33889077+lkissack@users.noreply.github.com>
2019-07-31 16:48:46 -0700

    Update iir_optimized.s

--- IIR/iir_optimized.s
+++ IIR/iir_optimized.s
@@ -480,9 +480,9 @@ main:
 	ldr	r3, [fp, #-48]	@ tmp237, tmp_4
 	qadd	r3, r2, r3	@ _35, _34, tmp237
 	movw	r2, #32767	@ tmp238,
-@	cmp	r3, r2	@ _35, tmp238
-@	movlt	r3, r3	@ _36, _35
-@	movge	r3, r2	@ _36, tmp238
+	cmp	r3, r2	@ _35, tmp238
+	movlt	r3, r3	@ _36, _35
+	movge	r3, r2	@ _36, tmp238
 @ iir_optimized.c:93: 		Y[i] = (short int) SHORT_INT_CLIPPING(tmp_0 +tmp_1 + tmp_2 + tmp_3 + tmp_4);
 	sxth	r2, r3	@ _37, _36
 	movw	r3, #:lower16:Y	@ tmp239,
@@ -615,9 +615,9 @@ main:
 	ldr	r3, [fp, #-48]	@ tmp257, tmp_4
 	qadd	r3, r2, r3	@ _70, _69, tmp257
 	movw	r2, #32767	@ tmp258,
-@	cmp	r3, r2	@ _70, tmp258
-@	movlt	r3, r3	@ _71, _70
-@	movge	r3, r2	@ _71, tmp258
+	cmp	r3, r2	@ _70, tmp258
+	movlt	r3, r3	@ _71, _70
+	movge	r3, r2	@ _71, tmp258
 @ iir_optimized.c:110: 		Y[i+1] = (short int) SHORT_INT_CLIPPING(tmp_0 +tmp_1 + tmp_2 + tmp_3 + tmp_4);
 	add	r1, r4, #1	@ _72, i,
 @ iir_optimized.c:110: 		Y[i+1] = (short int) SHORT_INT_CLIPPING(tmp_0 +tmp_1 + tmp_2 + tmp_3 + tmp_4);
